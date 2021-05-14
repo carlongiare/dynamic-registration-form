5e65f237517f3ded7f4ac11a9b0b8ee260e0409f
carl <carl@limatech.co.ke>
2021-05-14 22:07:29 +0300

    initial commit: simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormArray} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'registration-form';
+  constructor(private form_builder: FormBuilder){}
+  public contactTypes = [{name:'Admin'},{name:'Manager'},{name:'Employee'},]
+  public documentTypes = [{name:'KRA PIN'},{name:'ID Number'},{name:'Passport Number'},]
+  userForm = this.form_builder.group({
+    first_name: ['', [Validators.pattern("[a-zA-Z ]*")]],
+    second_name: ['', [Validators.pattern("[a-zA-Z ]*")]],
+    last_name: ['', [Validators.pattern("[a-zA-Z ]*")]],
+    email: ['',[Validators.email]],
+    full_address: [''],
+    document_type: [''],
+    document_number:['']
+  });
+
+  businessContactInfoForm = this.form_builder.group({
+    contact_type:[''],
+    contact_name: ['', [Validators.pattern("[a-zA-Z ]*")]],
+    contact_position: [''],
+    contact_email: ['',[Validators.email]],
+    contact_mobile: [''],
+    direct_phone: ['']
+  });
+
+
+  onFileSelected(event: any){
+
+  }
+  addDocument(){
+
+  }
+  addShareholder(){
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +19,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
