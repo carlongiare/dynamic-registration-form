147959729877418a385b5ac438a6b635b8e91690
carl <carl@limatech.co.ke>
2021-05-15 06:50:58 +0300

    made documents form group dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormBuilder, Validators, FormArray} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray} from '@angular/forms';
 
 
 @Component({
@@ -11,14 +11,18 @@ export class AppComponent {
   constructor(private form_builder: FormBuilder){}
   public contactTypes = [{name:'Admin'},{name:'Manager'},{name:'Employee'},]
   public documentTypes = [{name:'KRA PIN'},{name:'ID Number'},{name:'Passport Number'},]
-  userForm = this.form_builder.group({
+  public businessTypes = [{name:'NGO'},{name:'LLC'},{name:'Co-Op'},]
+  public businessNature = [{name:'Hawking'},{name:'Retail Shop'},{name:'Service Shop'},]
+  public documents: any;
+  shareholdersInfoForm = this.form_builder.group({
     first_name: ['', [Validators.pattern("[a-zA-Z ]*")]],
     second_name: ['', [Validators.pattern("[a-zA-Z ]*")]],
     last_name: ['', [Validators.pattern("[a-zA-Z ]*")]],
     email: ['',[Validators.email]],
     full_address: [''],
-    document_type: [''],
-    document_number:['']
+    documents: this.form_builder.array([
+      this.addDocumentFormGroup()
+    ])
   });
 
   businessContactInfoForm = this.form_builder.group({
@@ -30,15 +34,40 @@ export class AppComponent {
     direct_phone: ['']
   });
 
+  businessDetailsForm = this.form_builder.group({
+    business_type:[''],
+    organization_name: [''],
+    permit_number: [''],
+    business_nature: [''],
+    physical_location: [''],
+    postal_address: [''],
+    city: [''],
+    kra_pin: ['']
+  });
+
+  ngOnInit(){
+    this.documents = <FormArray>this.shareholdersInfoForm.get('documents');
+  }
+
 
   onFileSelected(event: any){
 
   }
   addDocument(){
-
+    (<FormArray>this.shareholdersInfoForm.get('documents')).push(this.addDocumentFormGroup());
+  }
+  removeDocument(i:any){
+    (<FormArray>this.shareholdersInfoForm.get('documents')).removeAt(i);
   }
   addShareholder(){
     
   }
+  addDocumentFormGroup(): FormGroup{
+    return this.form_builder.group({
+      document_type: [''],
+      document_number:['']  
+    });
+  }
+
 
 }
